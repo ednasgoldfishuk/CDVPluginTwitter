a775c3a842f97ef5997e08f3fcfdc80666562302
Phil Lidstone <plidstone@gmail.com>
2013-11-05 15:19:23 +0000

    JS: remove defintion

--- www/PluginTwitter.js
+++ www/PluginTwitter.js
@@ -1,13 +1,3 @@
-cordova.define("org.apache.cordova.file.File", function(require, exports, module) {
-/**
- * cordova is available under *either* the terms of the modified BSD license *or* the
- * MIT License (2008). See http://opensource.org/licenses/alphabetical for full text.
- *
- * Copyright (c) Matt Kane 2010
- * Copyright (c) 2011, IBM Corporation
- */
-               
-var exec = require('cordova/exec');
 
 /**
  * Constructor.
@@ -150,5 +140,5 @@ PluginTwitter.prototype.composeTweet = function(successCallback, errorCallback, 
     cordova.exec(success, failure, "TwitterPlugin", "composeTweet", [options]);
 };
 
-module.exports = new PluginTwitter();
-});
+//module.exports = new PluginTwitter();
+var PluginTwitter = new PluginTwitter();
